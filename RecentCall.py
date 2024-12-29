83054ff14a0485fa9ebdca95fdbfefffc9970877
TerryNguyen1403 <giangnht1403@gmail.com>
2024-12-29 18:51:15 +0700

    RecentCall solution

---
+++ RecentCall.py
@@ -0,0 +1,14 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q and self.q[0] < t - 3000:
+            self.q.popleft()
+
+        return len(self.q)
